01027fd319a8f301b8fcca6a416891e412a6f17c
vmetzger <vladii@me.com>
2021-11-05 22:45:49 +0100

    a

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogoutComponent } from './components/logout/logout.component';
+
+
+
+@NgModule({
+  declarations: [
+    LogoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
